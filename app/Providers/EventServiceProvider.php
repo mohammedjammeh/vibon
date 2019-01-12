ec457317269b5ded2a846ccb4aeb9a6f9561be43
Mohammed Jammeh <>
2019-01-12 22:54:12 +0000

    Added sending email to newly registered users as a way to practice setting up custom events and listeners.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use App\Events\UserCreated;
+use App\Listeners\SendUserCreatedNotification;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +19,10 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+
+        UserCreated::class => [
+            SendUserCreatedNotification::class,
         ],
     ];
 
