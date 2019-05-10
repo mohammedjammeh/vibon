9746b851587bc57adbbebab3f4c9d9f5c237ddef
Mohammed Jammeh <>
2019-05-10 09:24:52 +0100

    Moved AutoDJ tests from Feature to Unit tests and removed some example test files.

--- tests/Unit/AutoDJ/TracksTest.php
+++ tests/Unit/AutoDJ/TracksTest.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace Tests\Feature\AutoDJ;
+namespace Tests\Unit\AutoDJ;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\User;
 use App\Vibe;
 use App\Track;
 use App\AutoDJ\Tracks as AutoTracks;
@@ -18,14 +19,17 @@ class TracksTest extends TestCase
 
     public function test_auto_tracks_of_a_vibe_which_belongs_to_its_users_can_be_stored()
     {
+        $user = factory(User::class)->create();
+        $tracks = factory(Track::class, 2)->create();
+        $tracksIDs = $tracks->pluck('id')->toArray();
         $vibe = factory(Vibe::class)->create();
+        $user->tracks()->attach($tracksIDs, ['type' => UserAPI::TOP_TRACK]);
+        $vibe->users()->attach($user->id, ['owner' => true]);
+
         AutoTracks::store($vibe);
-
         $vibeAutoTracks = $vibe->tracks()->where('auto_related', true)->get()->pluck('api_id');
-
 		$vibeLoad = $vibe->load('users.tracks');
 		$vibeUsersTracks = $vibeLoad['tracks']->pluck('api_id');
-		
 		foreach ($vibeAutoTracks as $autoTrack) {
 			$this->assertContains($autoTrack, $vibeUsersTracks);
 		}
@@ -33,16 +37,20 @@ class TracksTest extends TestCase
 
     public function test_auto_tracks_of_a_vibe_which_belongs_to_its_users_can_be_updated()
     {
+        $user = factory(User::class)->create();
     	$vibe = factory(Vibe::class)->create();
-    	event(new VibeCreated($vibe));
+        $vibe->users()->attach($user->id, ['owner' => true]);
 
-    	$vibeUser = $vibe->users->first();
-    	$newUserTrack = factory(Track::class)->create();
-    	$vibeUser->tracks()->attach($newUserTrack->id, ['type' => UserAPI::TOP_TRACK]);
+        $vibeTrack = factory(Track::class)->create();
+        $vibe->tracks()->attach($vibeTrack->id, ['auto_related' => true]);
 
+    	$userTrack = factory(Track::class)->create();
+    	$user->tracks()->attach($userTrack->id, ['type' => UserAPI::TOP_TRACK]);
+
+        event(new VibeCreated($vibe));
     	AutoTracks::update($vibe);
-
     	$vibeAutoTracks = $vibe->tracks()->where('auto_related', true)->get()->pluck('api_id');
-    	$this->assertContains($newUserTrack->api_id, $vibeAutoTracks);
+        $this->assertContains($userTrack->api_id, $vibeAutoTracks);
+    	$this->assertNotContains($vibeTrack->api_id, $vibeAutoTracks);
     }
 }
--- tests/Unit/AutoDJ/UserTest.php
+++ tests/Unit/AutoDJ/UserTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\AutoDJ;
+namespace Tests\Unit\AutoDJ;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
