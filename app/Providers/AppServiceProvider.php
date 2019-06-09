93b691202985309f8da5330ace106f17932c7246
Mohammed Jammeh <>
2019-06-09 16:56:07 +0100

    - Remove SpotifyServiceProvider and move it's functionality to AppServiceProvider - Add test for for View share of 'user' in AppServiceProvider - More refactoring

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use App\Music\InterfaceAPI;
+use App\Music\Spotify\WebAPI;
+use SpotifyWebAPI\Session;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +29,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(InterfaceAPI::class, WebAPI::class);
+        $this->app->singleton('SpotifySession', function () {
+            $spotifySession = new Session(
+                config('services.spotify.client_id'),
+                config('services.spotify.client_secret'),
+                config('services.spotify.redirect')
+            );
+            return $spotifySession;
+        });
     }
 }
---
+++ tests/Unit/Providers/AppServiceProviderTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\Providers;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Music\Playlist;
+use App\Vibe;
+
+class AppServiceProviderTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->app->extend('SpotifySession', function () {
+            return new Session();
+        });
+    }
+
+    public function test_all_views_have_the_user_variable()
+    {
+        $user = $this->user->load('vibes.tracks');
+        app(Playlist::class)->loadMany($user['vibes']);
+        $home = $this->get(route('index'));
+        $home->assertViewHas('user', $user);
+
+        $vibe = factory(Vibe::class)->create();
+        $vibeShowPage = $this->get($vibe->path);
+        $vibeShowPage->assertViewHas('user', $user);
+    }
+}
