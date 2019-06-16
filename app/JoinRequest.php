da3ab5881b71ddaf387730599bc52d7574f94115
Mohammed Jammeh <>
2019-06-16 15:15:45 +0100

    Add vibe belongsTo relationship to JoinRequest

--- app/JoinRequest.php
+++ app/JoinRequest.php
@@ -12,4 +12,9 @@ class JoinRequest extends Model
     {
     	return $this->belongsTo(User::class);
     }
+
+    public function vibe()
+    {
+        return $this->belongsTo(Vibe::class);
+    }
 }
