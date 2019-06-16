6e479bab1f1b96af4de5fbe6d17f261d0ec19646
Mohammed Jammeh <>
2019-06-16 20:17:04 +0100

    Add unit tests(model and controller) for Vibe

---
+++ tests/Unit/Controllers/VibeTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tests\Unit\Controllers;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use App\User;
+use App\Vibe;
+use App\Track;
+use App\MusicAPI\Playlist;
+use App\Events\VibeCreated;
+use App\Events\VibeUpdated;
+use App\MusicAPI\User as UserAPI;
+use App\MusicAPI\Tracks;
+
+
+class VibeTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    public function test_vibe_requires_a_name_to_be_created()
+    {
+        factory(Vibe::class)->raw(['name' => '']);
+        $this->post(route('vibe.store'))->assertSessionHasErrors('name');
+    }
+
+    public function test_vibe_requires_a_description_to_be_created()
+    {
+        factory(Vibe::class)->raw(['description' => '']);
+        $this->post(route('vibe.store'))->assertSessionHasErrors('description');
+    }
+
+    public function test_vibe_created_event_is_triggered_when_a_vibe_is_created()
+    {
+        Event::fake();
+        $playlist = app(Playlist::class)->create('Party');
+        $attributes = factory(Vibe::class)->raw([
+            'name' => $playlist->name,
+            'api_id' => $playlist->id
+        ]);
+        $this->post(route('vibe.store'), $attributes);
+        Event::assertDispatched(VibeCreated::class);
+    }
+
+    public function test_vibe_created_listener_ensures_that_new_vibe_has_auto_related_tracks_based_on_its_users_tracks()
+    {
+        $user = factory(User::class)->create();
+        $tracks = factory(Track::class, 2)->create();
+        $tracksIDs = $tracks->pluck('id')->toArray();
+        $user->tracks()->attach($tracksIDs, ['type' => UserAPI::TOP_TRACK]);
+        $vibe = factory(Vibe::class)->create();
+        $vibe->users()->attach($user->id, ['owner' => true]);
+
+        event(new VibeCreated($vibe));
+        $vibeAutoTracks = $vibe->tracks()->where('auto_related', true)->get()->pluck('api_id');
+        $vibeLoad = $vibe->load('users.tracks');
+        $vibeUsersTracks = $vibeLoad['tracks']->pluck('api_id');
+
+        foreach ($vibeAutoTracks as $autoTrack) {
+            $this->assertContains($autoTrack, $vibeUsersTracks);
+        }
+    }
+
+    public function test_vibe_show_view_gets_required_data()
+    {
+        $vibe = factory(Vibe::class)->create();
+        $tracks = $vibe->showTracks();
+        $loadedTracks = app(Tracks::class)->load($tracks);
+        $this->get($vibe->path)->assertViewHasAll([
+            'vibe' => app(Playlist::class)->load($vibe),
+            'apiTracks' => app(Tracks::class)->check($loadedTracks)
+        ]);
+    }
+
+    public function test_vibe_is_shown_with_the_right_view()
+    {
+        $vibe = factory(Vibe::class)->create();
+        $this->get($vibe->path)->assertViewIs('vibe.show');
+    }
+
+    public function test_vibe_updated_event_is_triggered_when_a_vibe_is_updated()
+    {
+        Event::fake();
+        $vibe = factory(Vibe::class)->create();
+        $user = factory(User::class)->create();
+        $vibe->users()->attach($user->id, ['owner' => true]);
+
+        $this->actingAs($vibe->users->first());
+        $this->patch(route('vibe.update', $vibe), [
+            'name' => 'Shaka Dance',
+            'description' => 'Shakala Boom Boom',
+            'open' => $vibe->open,
+            'auto_dj' =>  $vibe->auto_dj
+        ]);
+        Event::assertDispatched(VibeUpdated::class);
+    }
+}
---
+++ tests/Unit/Models/VibeTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Genre;
+use App\Track;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use App\Vibe;
+use App\JoinRequest;
+
+class VibeTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    public function test_the_show_tracks_method_gets_the_right_tracks()
+    {
+        $vibe = factory(Vibe::class)->create();
+
+        $autoTracks = factory(Track::class, 3)->create();
+        $autoTracks->map(function ($track) {
+            $genres = factory(Genre::class, 2)->create();
+            return $track->genres()->attach($genres->pluck('id'), []);
+        });
+        $vibe->tracks()->attach($autoTracks->pluck('id'), ['auto_related' => true]);
+
+        $tracks = factory(Track::class, 4)->create();
+        $vibe->tracks()->attach($tracks->pluck('id'), ['auto_related' => false]);
+
+        $vibe->auto_dj = true;
+        $vibe->save();
+        $this->assertEquals($vibe->showTracks()->count(), $autoTracks->count());
+        foreach ($vibe->showTracks() as $track) {
+            $this->assertContains($track->id, $autoTracks->pluck('id'));
+        }
+
+        $vibe->auto_dj = false;
+        $vibe->save();
+        $this->assertEquals($vibe->showTracks()->count(), $tracks->count());
+        foreach ($vibe->showTracks() as $track) {
+            $this->assertContains($track->id, $tracks->pluck('id'));
+        }
+    }
+
+    public function test_the_has_member_function_checks_if_a_user_is_a_member_of_a_vibe_or_not()
+    {
+        $vibe = factory(Vibe::class)->create();
+
+        $nonMember = factory(User::class)->create();
+        $this->assertEquals($vibe->hasMember($nonMember), false);
+
+        $newVibe = factory(Vibe::class)->create();
+        $member = factory(User::class)->create();
+        $newVibe->users()->attach($member->id, ['owner' => false]);
+        $this->assertEquals($newVibe->hasMember($member), true);
+    }
+
+    public function test_the_has_join_request_from_method_checks_if_a_vibe_has_a_join_request_from_a_user_or_not()
+    {
+        $vibe = factory(Vibe::class)->create();
+        $user = factory(User::class)->create();
+        $this->assertEquals($vibe->hasJoinRequestFrom($user), false);
+
+        $newVibe = factory(Vibe::class)->create();
+        $newUser = factory(User::class)->create();
+        factory(JoinRequest::class)->create([
+            'vibe_id' => $newVibe,
+            'user_id' => $newUser->id
+        ]);
+        $this->assertEquals($newVibe->hasJoinRequestFrom($newUser), true);
+    }
+
+    public function test_the_join_request_from_method_gets_the_join_request_from_given_user()
+    {
+        $vibe = factory(Vibe::class)->create();
+        $user = factory(User::class)->create();
+        $joinRequest = factory(JoinRequest::class)->create([
+            'vibe_id' => $vibe,
+            'user_id' => $user->id
+        ]);
+        $this->assertEquals($vibe->joinRequestFrom($user)->id, $joinRequest->id);
+        $this->assertEquals($vibe->joinRequestFrom($user)->vibe_id, $vibe->id);
+        $this->assertEquals($vibe->joinRequestFrom($user)->user_id, $user->id);
+    }
+
+    public function test_the_get_owner_attribute_method_gets_the_owner_of_a_vibe()
+    {
+        $vibe = factory(Vibe::class)->create();
+        $user = factory(User::class)->create();
+        $vibe->users()->attach($user->id, ['owner' => true]);
+
+        $this->assertEquals($vibe->owner->id, $user->id);
+        $this->assertEquals($vibe->owner->username, $user->username);
+    }
+
+    public function test_the_get_path_attribute_method_gets_the_path_of_a_vibe()
+    {
+        $vibe = factory(Vibe::class)->create();
+        $this->assertEquals($vibe->path, route('vibe.show', $vibe));
+    }
+}
