2031e9e8a9c5e3ba2c360811958989d3ed93292a
Mohammed Jammeh <mohammedjammeh@yahoo.com>
2019-12-08 01:25:21 +0000

    Add search route and component..

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,14 @@ import './bootstrap';
 import router from './routes';
 import vibes from './components/vibe/index';
 import createVibe from './components/vibe/create';
+import searchForm from './components/search/form';
 
 new Vue({
     el: '#app',
     components: {
         'vibes': vibes,
-        'create-vibe': createVibe
+        'create-vibe': createVibe,
+        'search-form': searchForm
     },
     data: {},
     router: router
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,11 +1,17 @@
 import VueRouter from 'vue-router';
 import show from './components/vibe/show';
+import searchResults from './components/search/results';
 
 let routes = [
     {
         path: '/vibe/:id',
         name: 'showVibe',
         component: show
+    },
+    {
+        path: '/search/:input',
+        name: 'searchResults',
+        component: searchResults
     }
 ];
 
