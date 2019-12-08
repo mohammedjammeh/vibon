5298ae97f4e0d159ea7b2ab0f0299a7b1abb3ee1
Mohammed Jammeh <>
2019-12-08 01:22:44 +0000

    Add Search to Vue.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -7,16 +7,9 @@ use App\MusicAPI\Tracks as TracksAPI;
 
 class SearchController extends Controller
 {
-//    public function search(TracksAPI $tracksAPI)
-//    {
-//        $input = request('search');
-//        if(is_null($input)) {
-//            return redirect()->back();
-//        }
-//
-//        $tracks = app(Search::class)->tracks($input);
-//        return view('search', [
-//            'apiTracks' => $tracksAPI->check($tracks)
-//        ]);
-//    }
+    public function search($input)
+    {
+        $tracks = app(Search::class)->tracks($input);
+        return  app(TracksAPI::class)->updateTracksVibonInfo($tracks);
+    }
 }
