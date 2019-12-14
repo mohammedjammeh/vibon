2fb71f3b36cb344c8715e5f30a088ca5ed8eed91
Mohammed Jammeh <>
2019-12-14 23:09:09 +0000

    Minor refactoring..

--- resources/js/core/search.js
+++ resources/js/core/search.js
@@ -1,8 +1,24 @@
 let Search = {
     route: '',
+    tracks: {},
+    playingTrack: '',
 
     setRoute: function (input) {
         this.route = '/search/' + input;
+    },
+
+    searchInput: function(input) {
+        this.setRoute(input);
+        return new Promise((resolve, reject) => {
+            axios.get(this.route)
+                .then(response => {
+                    this.tracks = response.data;
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    reject(error.response.data.errors);
+                });
+        });
     }
 }
 
