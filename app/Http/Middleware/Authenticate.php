cf0e9cf2e8b359b6eeae750e456d0d813d93451e
Mohammed Jammeh <>
2020-06-19 08:06:49 +0100

    Update authenticate middleware to return right route and not redirect.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return redirect(route('welcome'));
+            return route('welcome');
         }
     }
 }
