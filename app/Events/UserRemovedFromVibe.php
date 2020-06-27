c192bc63126c0bcb6917e618a889235234bf08e3
Mohammed Jammeh <mohammedjammeh@yahoo.com>
2020-06-27 13:37:50 +0100

    Add websockets broadcast events for UserVibeController

---
+++ app/Events/UserRemovedFromVibe.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class UserRemovedFromVibe implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $vibe;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param $vibe
+     * @return void
+     */
+    public function __construct($vibe)
+    {
+        $this->vibe = $vibe;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('user.removed.from.vibe');
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'vibe' => $this->vibe->id
+        ];
+    }
+}
--- app/Http/Controllers/UserVibeController.php
+++ app/Http/Controllers/UserVibeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\UserJoinedVibe;
+use App\Events\UserLeftVibe;
+use App\Events\UserRemovedFromVibe;
 use App\Traits\VibeShowTrait;
 use App\Vibe;
 use App\User;
@@ -16,6 +19,9 @@ class UserVibeController extends Controller
     {
         $vibe->users()->attach(auth()->user()->id, ['owner' => false]);
 
+        broadcast(new UserJoinedVibe($vibe))->toOthers();
+        // notify vibe owner
+
         $loadedVibe = app(Playlist::class)->load($vibe);
         $message = 'Welcome to ' .  $loadedVibe->name . '.';
         return $this->showResponse($loadedVibe, $message);
@@ -26,6 +32,9 @@ class UserVibeController extends Controller
         $user = auth()->user();
         $vibe->users()->detach($user->id);
 
+        broadcast(new UserLeftVibe($vibe))->toOthers();
+        // notify vibe owner
+
         $loadedVibe = app(Playlist::class)->load($vibe);
         $message = 'You are no longer part of ' . $loadedVibe->name . '.';
         return $this->showResponse($loadedVibe, $message);
@@ -35,6 +44,9 @@ class UserVibeController extends Controller
     {
         $user->notify(new RemovedFromAVibe($vibe->id));
     	$vibe->users()->detach($user->id);
+
+        broadcast(new UserRemovedFromVibe($vibe))->toOthers();
+        // notify vibe owner
 
         $loadedVibe = app(Playlist::class)->load($vibe);
         $message = $user->username . ' is no longer a member of ' . $loadedVibe->name . '.';
