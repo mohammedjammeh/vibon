5d6a900b7b9d23ec9b57681d61478fab54db324d
Mohammed Jammeh <mohammedjammeh@yahoo.com>
2020-07-02 15:29:04 +0100

    Remove notifications display on home and search blade files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,26 +5,6 @@
         @if(session('message'))
             <p>{{ session('message') }}</p>
         @endif
-
-        {{--@if(count($user->notifications) > 0)--}}
-            {{--<h3>Notifications</h3>--}}
-            {{--@foreach($user->notifications as $notification)--}}
-                {{--@foreach($vibes as $vibe)--}}
-                    {{--@if($vibe->id == $notification->data['vibe_id'])--}}
-                        {{--@if($notification->type == 'App\Notifications\ResponseToJoinAVibe')--}}
-                            {{--@if($notification->data['response'])--}}
-                                {{--<p>Your request to join '{{ $vibe->name }}' has been accepted.</p>--}}
-                            {{--@else--}}
-                                {{--<p>Sorry, your request to join '{{ $vibe->name }}' has been rejected.</p>--}}
-                            {{--@endif--}}
-                        {{--@elseif($notification->type == 'App\Notifications\RemovedFromAVibe')--}}
-                            {{--<p>You have been removed from the '{{ $vibe->name }}' vibe.</p>--}}
-                        {{--@endif--}}
-                    {{--@endif--}}
-                {{--@endforeach--}}
-            {{--@endforeach--}}
-        {{--@endif--}}
-        <br>
     
         @if(!empty($user))
             @if(count($user['vibes']) > 0)
