ee5342e4cb85efd7b41fa02efe89d86d999f2c92
Mohammed Jammeh <mohammedjammeh@yahoo.com>
2020-12-22 19:59:19 +0000

    Refactoring the api loading process of vibe tracks.

--- app/Traits/VibeShowTrait.php
+++ app/Traits/VibeShowTrait.php
@@ -2,20 +2,13 @@
 
 namespace App\Traits;
 
+use App\PendingVibeTrack;
 use App\Policies\VibePolicy;
+use App\Track;
 
 trait VibeShowTrait
 {
     public function showResponse($loadedVibe, $message = '')
-    {
-        $this->updateAttributes($loadedVibe);
-        return [
-            'vibe' => $loadedVibe,
-            'message' => $message
-        ];
-    }
-
-    public function updateAttributes($loadedVibe)
     {
         $loadedVibe->destroyable = App(VibePolicy::class)->delete(auth()->user(), $loadedVibe);
         $loadedVibe->requests = $loadedVibe->joinRequests;
@@ -23,5 +16,62 @@ trait VibeShowTrait
         $loadedVibe->hasJoinRequestFromUser = $loadedVibe->hasJoinRequestFrom(auth()->user());
         $loadedVibe->joinRequestFromUser = $loadedVibe->joinRequestFrom(auth()->user());
         $loadedVibe->notifications = $loadedVibe->notifications();
+        $loadedVibe->api_tracks = $loadedVibe->api_tracks->map(function($loadedTrack) use($loadedVibe) {
+            return $this->updateTrackInfo($loadedTrack, $loadedVibe);
+        });
+
+        return ['vibe' => $loadedVibe, 'message' => $message];
+    }
+
+    public function updateTracksVibonInfo($apiTracks)
+    {
+        return collect($apiTracks)->each(function ($apiTrack) {
+            return $this->updateTrackVibonInfo($apiTrack);
+        });
+    }
+
+    protected function updateTrackInfo($loadedTrack, $vibe)
+    {
+        $track = $vibe->tracks->where('api_id', $loadedTrack->id)->first();
+        $loadedTrack->votes_count = $track->votesCountOn($vibe);
+        $loadedTrack->is_voted_by_user = $track->isVotedByAuthUserOn($vibe);
+        $loadedTrack = $this->updateTrackVibonInfo($loadedTrack);
+        return $loadedTrack;
+    }
+
+    protected function updateTrackVibonInfo($loadedTrack)
+    {
+        $loadedTrack->vibon_id = $this->getVibonID($loadedTrack);
+        $loadedTrack->vibes = $this->getVibesIDs($loadedTrack);
+        $loadedTrack->pending_vibes = PendingVibeTrack::where('track_id', $loadedTrack->vibon_id)
+            ->pluck('vibe_id')->toArray();
+
+        return $loadedTrack;
+    }
+
+    protected function getVibonID($loadedTrack)
+    {
+        $track = Track::where('api_id', $loadedTrack->id)->first();
+        return !is_null($track) ? $track->id : null;
+    }
+
+    protected function getVibesIDs($loadedTrack)
+    {
+        $user = $this->userVibesManualTracks();
+        $vibesTracks = $user['vibes']->pluck('tracks', 'id');
+
+        $userTracksVibes = $vibesTracks->filter(function ($tracks) use($loadedTrack) {
+            $tracksIDs = $tracks->pluck('api_id');
+            return $tracksIDs->contains($loadedTrack->id);
+        });
+
+        return $userTracksVibes->keys()->toArray();
+    }
+
+    protected function userVibesManualTracks()
+    {
+        return auth()->user()->load(['vibes.tracks' => function($query) {
+            $query->where('auto_related', false);
+        }]);
     }
 }
--- tests/Unit/MusicAPI/TracksTest.php
+++ tests/Unit/MusicAPI/TracksTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\MusicAPI;
 
+use App\MusicAPI\Playlist;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -13,16 +14,36 @@ class TracksTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
+    public function test_the_tracks_load_for_method_returns_loaded_tracks_of_a_vibe()
+    {
+        $vibe = factory(Vibe::class)->create(['auto_dj' => false]);
+        $playlist = app(Playlist::class)->get($vibe->api_id);
+        $tracks = factory(Track::class, 2)->create();
+        $vibe->tracks()->attach($tracks->pluck('id'), ['auto_related' => false]);
+
+        $tracksAPI = app(Tracks::class)->loadFor($vibe, $playlist);
+
+        collect($tracksAPI)->each(function ($trackAPI){
+            $this->assertObjectHasAttribute('name', $trackAPI);
+            $this->assertObjectHasAttribute('album', $trackAPI);
+            $this->assertObjectHasAttribute('artists', $trackAPI);
+            $this->assertObjectHasAttribute('uri', $trackAPI);
+        });
+    }
+    
     public function test_the_tracks_load_method_loads_all_tracks_from_the_api_based_on_given_tracks_from_database()
     {
         $tracks = factory(Track::class, 2)->create();
         $tracksIDsForAPI = $tracks->pluck('api_id');
+
         $tracksAPI = app(Tracks::class)->load($tracks);
 
         collect($tracksAPI)->each(function ($trackAPI) use($tracksIDsForAPI) {
             $this->assertContains($trackAPI->id, $tracksIDsForAPI);
             $this->assertObjectHasAttribute('name', $trackAPI);
             $this->assertObjectHasAttribute('artists', $trackAPI);
+            $this->assertObjectHasAttribute('artists', $trackAPI);
+            $this->assertObjectHasAttribute('uri', $trackAPI);
         });
     }
 }
