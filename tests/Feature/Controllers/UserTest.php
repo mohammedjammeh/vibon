fd338b697fee549072e57d9f7105e393473074d3
Mohammed Jammeh <mohammedjammeh@yahoo.com>
2021-04-04 10:52:12 +0100

    Remove superfluous user controller test.

--- tests/Feature/Controllers/UserTest.php
+++ tests/Feature/Controllers/UserTest.php
@@ -12,20 +12,6 @@ class UserTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
-    public function test_vibes_method_returns_current_users_vibes_ids()
-    {
-        $user = factory(User::class)->create();
-        $autoVibe = factory(Vibe::class)->create(['auto_dj' => true]);
-        $manualVibe = factory(Vibe::class)->create(['auto_dj' => false]);
-        $user->vibes()->attach([$autoVibe->id, $manualVibe->id]);
-
-        $this->actingAs($user);
-        $response = $this->get(route('user.vibes'))->baseResponse->original;
-
-        $this->assertEquals($autoVibe->id, $response['auto']->first());
-        $this->assertEquals($manualVibe->id, $response['manual']->first());
-    }
-
     public function test_attributes_method_returns_expected_user_data()
     {
         $user = factory(User::class)->create();
